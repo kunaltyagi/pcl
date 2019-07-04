ee835cfccf6f2593e05ab3b005daeaa1120c261f
Kunal Tyagi <tyagi.kunal@live.com>
2019-07-04 13:22:25 +0900

    Adding shell script for clang-format

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,31 +1,10 @@
-# select all files with correct c++ extensions
-file(GLOB_RECURSE
-  ALL_CXX_SOURCE_FILES
-  *.[ch] *.[chi]pp *.[chi]xx *.cppm *.cc *.hh *.ii *.[CHI]
-)
-
-# exclude the docs **for now**
-set(EXCLUDE_REGEX "${PCL_SOURCE_DIR}[\\|/]doc[\\|/].*")
-if(CMAKE_VERSION VERSION_LESS "3.6.0")
-  foreach(PROPOSED_FILE ${ALL_CXX_SOURCE_FILES})
-    string(REGEX MATCH ${EXCLUDE_REGEX} regex_found ${PROPOSED_FILE})
-    if(regex_found)
-      list(REMOVE_ITEM ALL_CXX_SOURCE_FILES ${PROPOSED_FILE})
-    endif()
-  endforeach()
-else()
-  list(FILTER ALL_CXX_SOURCE_FILES EXCLUDE REGEX ${EXCLUDE_REGEX})
-endif()
-
 find_package(ClangFormat 6)
 # search for version number in clang-format without version number
 if(ClangFormat_FOUND)
   message(STATUS "Adding target 'format'")
   add_custom_target(
   format
-  COMMAND ${ClangFormat_EXECUTABLE}
-    -i
-    -style=file
-    ${ALL_CXX_SOURCE_FILES}
+  COMMAND ${PCL_SOURCE_DIR}/dev/format.sh
+    ${ClangFormat_EXECUTABLE}
   )
 endif()
